0ac82c04c0165c17522dc750add68bfe0053032a
Emily Koceja <117419130+eskoceja@users.noreply.github.com>
2023-05-09 11:26:02 -0400

    calculations assignment

---
+++ calculations/src/Calculations.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int numOne = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Enter another number: ");
+        int numTwo = Integer.valueOf(scanner.nextLine());
+
+        int addition = numOne + numTwo;
+        int subtraction = numOne - numTwo;
+        int multiplication = numOne * numTwo;
+        double division = numOne / numTwo;
+        int modulus = numOne % numTwo;
+
+        System.out.println("Addition: " + numOne + " + " + numTwo + " = " + addition);
+        System.out.println("Subtraction: " + numOne + " - " + numTwo + " = " + subtraction);
+        System.out.println("Multiplication: " + numOne + " * " + numTwo + " = " + multiplication);
+        System.out.println("Division: " + numOne + " / " + numTwo + " = " + division);
+        System.out.println("Modulus: " + numOne + " % " + numTwo + " = " + modulus);
+    }
+}
